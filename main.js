10009870a8f30a072a5f5f2382d8103e6cbc8195
Izac <izacjunior@gmail.com>
2020-02-25 20:02:59 -0300

    Tester

---
+++ main.js
@@ -0,0 +1,20 @@
+
+var welcome = "Hello javaScript - Automação"
+document.getElementById("welcome").append(welcome)
+
+console.log("Hello javaScript - Automação")
+
+//variavaeis
+
+var number = 10;
+console.log(number)
+//Responsavel por trazer tipo da variavel
+console.log(typeof number)
+
+var name= "Fernando"
+console.log(typeof name)
+
+var n1 = 10;
+var n2 = 20;
+console.log(n1 + n2);
+
